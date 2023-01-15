312abaf50394d3a4927dfe7297e73fd6c7768810
Alex <astephbi@gmail.com>
2023-01-15 09:30:46 +0000

    navbar done

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Image,
   Box,
+  Text,
   Heading,
   Flex,
   Center,
@@ -19,8 +20,14 @@ const HeroSection = () => {
             fontSize="60px"
             lineHeight="66px"
           >
-            Get your child a dedicated learning tab
+            Get your child a dedicated
+            <span style={{ color: 'secondary' }}> learning tab</span>
           </Heading>
+          <Text mt="10px">
+            A personal learning tablet for children in primary and
+            secondary school to learn anywhere, anytime, and excel
+            academically.
+          </Text>
         </Box>
         <Spacer />
         <Box>
